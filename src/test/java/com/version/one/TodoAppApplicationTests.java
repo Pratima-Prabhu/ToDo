b6f4c217cb8c330855c791c3eaaae3cfe9c0bdfa
pratima-prabhu <pratimaprabhu7@gmail.com>
2020-02-18 12:16:42 +0530

    initial commit

---
+++ src/test/java/com/version/one/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.version.one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
